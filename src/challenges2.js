8568923d0d5001c90a9c87e9fdda50edbad515fc
GPA1992 <seuemail@exemplo.br>
2022-05-22 17:37:00 -0300

    testando o exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,34 +1,27 @@
-// Desafio 11 Implemente a função generatePhoneNumber que recebe um array com 11 números e retorna um número de telefone, respeitando parênteses, traços e espaços.
+// Desafio 11 
+function generatePhoneNumber(numeros) {
+var numArr = [];
+const lista = numArr.concat(numeros);
 
-//Retorne a string 'Array com tamanho incorreto.' caso o array tenha o tamanho diferente de 11;
-//Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum dos números do array seja menor que 0;
-//Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array seja maior que 9;
-//Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array se repetir 3 vezes ou mais;
-//Retorne um número de telefone, respeitando parênteses, traços e espaços caso os números do array estejam de acordo com as especificações.
-
-function generatePhoneNumber(arr) {
-
-for (let i = 0; i < arr.length; i++){
-  const arrTam = arr.length;
-  if (arrTam !== 11){
+for (let i = 0; i < lista.length; i++){
+  
+  if (lista.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  else if(arr.filter(x => x === arr[i]) < 3 === true ){
+  else if(lista.filter(x => x === lista[i]) <= 3 === true ){
     return 'não é possível gerar um número de telefone com esses valores3';
   }
-  else if (Math.max(...arr) > 9) {
-  return 'não é possível gerar um número de telefone com esses valores';
-  }
-  else if (Math.min(...arr) < 0){
+  else if (Math.max(...lista) > 9 || Math.min(...lista) < 0) {
   return 'não é possível gerar um número de telefone com esses valores';
   }
   else {
-  return '('+ arr[0]+ arr[1] + ') '+ arr[2] + arr[3] + arr[4] + arr[5] + arr[6]+'-'+ arr[7] + arr[8] + arr[9] + arr[10]
+  return '('+ lista[0]+ lista[1] + ') '+ lista[2] + lista[3] + lista[4] + lista[5] + lista[6]+'-'+ lista[7] + lista[8] + lista[9] + lista[10]
   } 
 
   }
 }
 
+console.log(generatePhoneNumber([1, 2, 8, 0, 5, 3, 7, 8, 8, 1, 9]));
 
 // Desafio 12
 function triangleCheck(lineA, libeB, lineC) {
