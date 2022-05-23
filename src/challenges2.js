b70beea20f31dd1b9d851a6a8acc082b8755b2df
GPA1992 <seuemail@exemplo.br>
2022-05-23 10:20:00 -0300

    Faltando apenas o 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,36 +1,62 @@
-// Desafio 11 
+// Desafio 11
 function generatePhoneNumber(numeros) {
-var numArr = [];
-const lista = numArr.concat(numeros);
-
-for (let i = 0; i < lista.length; i++){
   
-  if (lista.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
-  else if(lista.filter(x => x === lista[i]) <= 3 === true ){
-    return 'não é possível gerar um número de telefone com esses valores3';
-  }
-  else if (Math.max(...lista) > 9 || Math.min(...lista) < 0) {
-  return 'não é possível gerar um número de telefone com esses valores';
-  }
-  else {
-  return '('+ lista[0]+ lista[1] + ') '+ lista[2] + lista[3] + lista[4] + lista[5] + lista[6]+'-'+ lista[7] + lista[8] + lista[9] + lista[10]
+  
+  for (let i= 0; i < 11; i++){
+
+    if (numeros.length !== 11) {
+      return 'Array com tamanho incorreto.';
+    }
+    else if (Math.max(...numeros) > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } else if (Math.min(...numeros) < 0){
+      return 'não é possível gerar um número de telefone com esses valores';
+    } 
+    else if(numeros.filter(x => x === numeros.length) < 3 === true ){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+     else {
+       return '(' + numeros[0] + numeros[1] + ') ' + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6] + '-' + numeros[7] + numeros[8] + numeros[9] + numeros[10]
+    }
   } 
-
-  }
+    
 }
-
-console.log(generatePhoneNumber([1, 2, 8, 0, 5, 3, 7, 8, 8, 1, 9]));
-
 // Desafio 12
-function triangleCheck(lineA, libeB, lineC) {
- 
+function triangleCheck(lineA, lineB, lineC) {
+const somaLadoAb = Math.abs(lineA + lineB); 
+const somaLadoBc = Math.abs(lineB + lineC); 
+const somaLadoCa = Math.abs(lineC + lineA);
+
+const subLadoAb = Math.abs(lineB - lineA);
+const subLadoBc = Math.abs(lineC - lineA);
+const subLadoCa = Math.abs(lineC - lineB);
+
+  if (lineA > somaLadoBc || lineB > somaLadoCa || lineC > somaLadoAb ){
+  return false
+  } else if (lineA < subLadoBc || lineB < subLadoCa || lineC < subLadoAb ){
+  return false;
+  } else {
+  return true
+  }
+
 }
+
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let reg = /\d+/g;
+  let result = frase.match(reg);
+    let arrNumberFraseSoma = 0;
+
+  for (let i = 0; i<result.length; i++){
+    arrNumberFraseSoma += Math.abs(result[i]);
+  }
+  if (arrNumberFraseSoma > 1) {
+    return arrNumberFraseSoma + ' copos de água'
+  } else  {
+    return arrNumberFraseSoma + ' copo de água'
+  }
+  
 }
 
 module.exports = {
